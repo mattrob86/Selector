363168e06b05d0e316feb7e3cf7ece60f8565f48
unknown <mattrob86@aol.com>
2020-02-19 21:55:39 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Shape from './shape';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Selector extends Component {
+
+constructor (){
+super ();
+this.state = {
+  selectedShape: "square",
+  selectedShape: "circle",
+  selectedShape: "triangle",
+  }
 }
 
-export default App;
+// this is where our methods  live
+
+selectedShape = (shapeName) => {
+  this.setShape({
+    selectedShape: shapeName
+  })
+}
+
+
+render(){
+  return (
+    <div className= 'container'>
+    <div className= 'navbar'>
+    <div> Selected: <span> {this.state.selectedShape}</span> </div>
+
+    </div>
+    <div className="shape-list">
+    <Shape shape="square"/>
+    <Shape shape="circle"/>
+    <Shape shape="triangle"/>
+    </div>
+    </div>
+  )
+}
+
+}
+export default Selector
